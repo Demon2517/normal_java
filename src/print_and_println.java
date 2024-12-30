bc228ac42dd68850f88bafbc0b03d3f8b546bc67
Demon2517 <rumitsolanki307@gmail.com>
2024-12-30 22:58:35 +0530

    FirstClass

---
+++ src/print_and_println.java
@@ -0,0 +1,13 @@
+public class print_and_println {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+        System.out.println("And");
+
+        System.out.print("*\n**\n***\n****\n*****");
+    }
+}
---
+++ src/take_in_sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class take_in_sum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+        //int sum = first + second;
+
+        System.out.print(first + second);
+
+    }
+}

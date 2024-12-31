f002042e3a851c9265b1dc830d3e0ae7005de860
Demon2517 <rumitsolanki307@gmail.com>
2024-12-31 22:50:28 +0530

    Loops In Java

---
+++ src/Loops.java
@@ -0,0 +1,26 @@
+public class Loops {
+    public static void main(String[] args) {
+
+        //For Loop
+        for (int i = 0; i < 10; i++){
+            System.out.println(i+1);
+        }
+
+        //While Loop
+        /*int i = 0;
+        while (i < 10) {
+            System.out.println(i+1);
+            i++;
+        }*/
+
+        //Do While Loop
+        /*
+        int i = 0;
+        do {
+            System.out.println(i+1);
+            i++;
+        } while (i < 10);
+
+         */
+    }
+}

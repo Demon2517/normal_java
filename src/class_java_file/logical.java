166a93ff466185abd52d5e27f2de69c0361ac898
Demon2517 <rumitsolanki307@gmail.com>
2025-02-07 23:33:14 +0530

    Delete src/class_java_file directory
    SOME mistake

--- src/class_java_file/logical.java
+++
@@ -1,8 +0,0 @@
-public class logical {
-    public static void main(String[] args) {
-        int a=20, b=10;
-        System.out.println(a>b && a<b);
-        System.out.println(a>b || a<b);
-        System.out.println(!(a>b && a<b));
-    }
-}
